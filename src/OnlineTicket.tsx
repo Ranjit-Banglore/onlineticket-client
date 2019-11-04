1941161750d4bce806fe0b10b4c7813db11c739c
ranjit <ranjit.patel@billpay.de>
2019-11-04 23:24:01 +0100

    MAI-002 added table to display data

--- src/OnlineTicket.tsx
+++ src/OnlineTicket.tsx
@@ -1,4 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import * as React from 'react';
+import { Table } from 'react-bootstrap';
 import './App.css';
 
 
@@ -11,6 +13,8 @@ class OnlineTicket extends React.Component<{}, any> {
 			tickets: []
 			
 		}
+		
+		this.renderTable = this.renderTable.bind(this);
 	}
 	
 	public componentDidMount() {
@@ -18,6 +22,17 @@ class OnlineTicket extends React.Component<{}, any> {
 		fetch("http://localhost:8080/api/tickets/alltickets")
 			.then(response => response.json())
 			.then(data => this.setState({tickets: data, isLoading: false}))
+	}
+	
+	public renderTable( a: any ): JSX.Element {
+		return <tr>
+			<td>{ a.ticketId }</td>
+			<td>{ a.passengerName }</td>
+			<td>{ a.bookingDate }</td>
+			<td>{ a.sourceStation }</td>
+			<td>{ a.destStation }</td>
+			<td>{ a.email }</td>
+		</tr>
 	}
 	
 	public render() {
@@ -28,17 +43,22 @@ class OnlineTicket extends React.Component<{}, any> {
 		
 		return (
 			<>
-			<div className="OnlineTicketComponent">
-				<p>Hello from Online Ticket component!!</p>
-			</div>
-			<div>
-				<h2> Ticket List</h2>
-				{ tickets.map(( ticket: any ) =>
-					<div key="ticket.id">
-						{ ticket.passengerName }
-					</div>
-				) }
-			</div>
+			<h1> Passenger ticket details..</h1>
+			<Table striped={ true } bordered={ true } hover={ true } variant={ "dark" }>
+				<thead>
+				<tr>
+					<th>Ticket Id</th>
+					<th>Passenger Name</th>
+					<th>Booking Date</th>
+					<th>Source Station</th>
+					<th>Destination Station</th>
+					<th>Email</th>
+				</tr>
+				</thead>
+				<tbody>
+				{ tickets.map(( ticket: any ) => this.renderTable(ticket)) }
+				</tbody>
+			</Table>
 			
 			</>
 		)
